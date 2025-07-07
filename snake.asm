8da672b081c86c698f374433367a97a656261ea7
AndresSalazar19 <aasalazar@espol.edu.ec>
2025-07-06 22:01:52 -0500

    final

--- snake.asm
+++ snake.asm
@@ -14,8 +14,8 @@ yVel:            .word    0               # Velocidad en Y
 xPos:            .word    20              # Posición X de la cabeza de la serpiente
 yPos:            .word    13              # Posición Y de la cabeza de la serpiente
 tail:            .word    2000            # Posición de la cola en el bitmap
-appleX:          .word    4               # Posición X de la manzana
-appleY:          .word    10              # Posición Y de la manzana
+plasticX:          .word    4               # Posición X de la manzana
+plasticY:          .word    10              # Posición Y de la manzana
 sharkUp:     .word 0xFFAAAAAA   # Gris claro, dirección 00
 sharkDown:   .word 0xFFAAAAAB   # Gris claro, dirección 01
 sharkLeft:   .word 0xFFAAAAAC   # Gris claro, dirección 10
@@ -97,7 +97,7 @@ drawBorderColumn:
     syscall
 
     # Dibujo inicial de la manzana
-    jal    drawApple
+    jal    drawPlastic
 
 # Bucle de actualización del juego
 
@@ -119,80 +119,80 @@ gameUpdateLoop:
     beq    $t3, 0, moveUp          # Si no hay entrada, ir hacia arriba
 
 moveUp:
-	lw	$s3, sharkUp	# s3 = direction of snake
-	add	$a0, $s3, $zero	# a0 = direction of snake
+	lw	$s3, sharkUp	# s3 = direction of Shark
+	add	$a0, $s3, $zero	# a0 = direction of Shark
 	addi	$t5, $zero, 0		# set x velocity to zero
 	addi	$t6, $zero, -1	 	# set y velocity to -1
 	sw	$t5, xVel		# update xVel in memory
 	sw	$t6, yVel		# update yVel in memory
-	jal	updateSnake
-	jal 	updateSnakeHeadPosition
+	jal	updateShark
+	jal 	updateSharkHeadPosition
 	
 	j	exitMoving 	
 
 moveDown:
-	lw	$s3, sharkDown	# s3 = direction of snake
-	add	$a0, $s3, $zero	# a0 = direction of snake
+	lw	$s3, sharkDown	# s3 = direction of Shark
+	add	$a0, $s3, $zero	# a0 = direction of Shark
 	addi	$t5, $zero, 0		# set x velocity to zero
 	addi	$t6, $zero, 1 		# set y velocity to 1
 	sw	$t5, xVel		# update xVel in memory
 	sw	$t6, yVel		# update yVel in memory
-	jal	updateSnake
-	jal 	updateSnakeHeadPosition
+	jal	updateShark
+	jal 	updateSharkHeadPosition
 	
 	j	exitMoving 
 	
 moveRight:
-	lw	$s3, sharkRight	# s3 = direction of snake
-	add	$a0, $s3, $zero	# a0 = direction of snake
+	lw	$s3, sharkRight	# s3 = direction of Shark
+	add	$a0, $s3, $zero	# a0 = direction of Shark
 	addi	$t5, $zero, 1		# set x velocity to 1
 	addi	$t6, $zero, 0 		# set y velocity to zero
 	sw	$t5, xVel		# update xVel in memory
 	sw	$t6, yVel		# update yVel in memory
-	jal	updateSnake
-	jal 	updateSnakeHeadPosition
+	jal	updateShark
+	jal 	updateSharkHeadPosition
 	
 	j	exitMoving 
 
 moveLeft:
-	lw	$s3, sharkLeft	# s3 = direction of snake
-	add	$a0, $s3, $zero	# a0 = direction of snake
+	lw	$s3, sharkLeft	# s3 = direction of Shark
+	add	$a0, $s3, $zero	# a0 = direction of Shark
 	addi	$t5, $zero, -1		# set x velocity to -1
 	addi	$t6, $zero, 0 		# set y velocity to zero
 	sw	$t5, xVel		# update xVel in memory
 	sw	$t6, yVel		# update yVel in memory
-	jal	updateSnake
-	jal 	updateSnakeHeadPosition
+	jal	updateShark
+	jal 	updateSharkHeadPosition
 	j	exitMoving 
 	
 	
-# this function update the snake on the bitmap display and changes its velocity
+# this function update the Shark on the bitmap display and changes its velocity
 # Param 1 is the direction
 # code logic steps
-# updateSnake(colorDir) {
+# updateShark(colorDir) {
 #	getBitMapLocation;
 #	store color dir in bitMapLoction
-#	getDirection of snake
-# 	update velocity based on snake
-#	check if head == apple
-#		get random new apple coordinates
-#		draw apple on bitmap display
-#		exit updateSnake function
+#	getDirection of Shark
+# 	update velocity based on Shark
+#	check if head == Plastic
+#		get random new Plastic coordinates
+#		draw Plastic on bitmap display
+#		exit updateShark function
 #	check head != background color
 #		game over
 #	Remove tail from bit map display
 #	update new tail base upon tail direction
-#	exit updateSnake function
+#	exit updateShark function
 # }	
-updateSnake:
+updateShark:
 	addiu 	$sp, $sp, -24	# allocate 24 bytes for stack
 	sw 	$fp, 0($sp)	# store caller's frame pointer
 	sw 	$ra, 4($sp)	# store caller's return address
-	addiu 	$fp, $sp, 20	# setup updateSnake frame pointer
+	addiu 	$fp, $sp, 20	# setup updateShark frame pointer
 	
 	### DRAW HEAD
-	lw	$t0, xPos		# t0 = xPos of snake
-	lw	$t1, yPos		# t1 = yPos of snake
+	lw	$t0, xPos		# t0 = xPos of Shark
+	lw	$t1, yPos		# t1 = yPos of Shark
 	lw	$t2, xConversion	# t2 = 4
 	lw	$t3, yConversion
 	mult	$t1, $t3		
@@ -207,10 +207,10 @@ updateSnake:
 	
 	### Head location checks
 	li 	$t2, 0x00ff0000		# load red color
-	bne	$t2, $t4, headNotApple	# if head location is not the apple branch away
+	bne	$t2, $t4, headNotPlastic	# if head location is not the Plastic branch away
 	
-	jal 	newAppleLocation
-	jal	drawApple
+	jal 	newPlasticLocation
+	jal	drawPlastic
 	# Incrementar el score
     lw   $t7, score       # Cargar el score actual
     addi $t7, $t7, 1      # Incrementar en 1
@@ -231,10 +231,10 @@ updateSnake:
     syscall
 
 
-	j exitUpdateSnake
+	j exitUpdateShark
 
 	
-headNotApple:
+headNotPlastic:
 
 	li	$t2, 0xFF003366			# load light gray color
 	beq	$t2, $t4, validHeadSquare	# if head location is background branch away
@@ -253,40 +253,40 @@ validHeadSquare:
 	sw	$t3, 0($t2)		# replace tail with background color
 	
 	### update new Tail
-	lw	$t5, sharkUp			# load word snake up = 0x0000ff00
-	beq	$t5, $t4, setNextTailUp		# if tail direction and color == snake up branch to setNextTailUp
+	lw	$t5, sharkUp			# load word Shark up = 0x0000ff00
+	beq	$t5, $t4, setNextTailUp		# if tail direction and color == Shark up branch to setNextTailUp
 	
-	lw	$t5, sharkDown			# load word snake up = 0x0100ff00
-	beq	$t5, $t4, setNextTailDown	# if tail direction and color == snake down branch to setNextTailDown
+	lw	$t5, sharkDown			# load word Shark up = 0x0100ff00
+	beq	$t5, $t4, setNextTailDown	# if tail direction and color == Shark down branch to setNextTailDown
 	
-	lw	$t5, sharkLeft			# load word snake up = 0x0200ff00
-	beq	$t5, $t4, setNextTailLeft	# if tail direction and color == snake left branch to setNextTailLeft
+	lw	$t5, sharkLeft			# load word Shark up = 0x0200ff00
+	beq	$t5, $t4, setNextTailLeft	# if tail direction and color == Shark left branch to setNextTailLeft
 	
-	lw	$t5, sharkRight			# load word snake up = 0x0300ff00
-	beq	$t5, $t4, setNextTailRight	# if tail direction and color == snake right branch to setNextTailRight
+	lw	$t5, sharkRight			# load word Shark up = 0x0300ff00
+	beq	$t5, $t4, setNextTailRight	# if tail direction and color == Shark right branch to setNextTailRight
 	
 setNextTailUp:
 	addi	$t0, $t0, -128		# tail = tail - 256
 	sw	$t0, tail		# store  tail in memory
-	j exitUpdateSnake
+	j exitUpdateShark
 	
 setNextTailDown:
 	addi	$t0, $t0, 128		# tail = tail + 256
 	sw	$t0, tail		# store  tail in memory
-	j exitUpdateSnake
+	j exitUpdateShark
 	
 setNextTailLeft:
 	addi	$t0, $t0, -4		# tail = tail - 4
 	sw	$t0, tail		# store  tail in memory
-	j exitUpdateSnake
+	j exitUpdateShark
 	
 setNextTailRight:
 	addi	$t0, $t0, 4		# tail = tail + 4
 	sw	$t0, tail		# store  tail in memory
-	j exitUpdateSnake
+	j exitUpdateShark
 	
 
-exitUpdateSnake:
+exitUpdateShark:
 	
 	lw 	$ra, 4($sp)	# load caller's return address
 	lw 	$fp, 0($sp)	# restores caller's frame pointer
@@ -294,11 +294,11 @@ exitUpdateSnake:
 	jr 	$ra		# return to caller's code
 	
 	
-updateSnakeHeadPosition:
+updateSharkHeadPosition:
 	addiu 	$sp, $sp, -24	# allocate 24 bytes for stack
 	sw 	$fp, 0($sp)	# store caller's frame pointer
 	sw 	$ra, 4($sp)	# store caller's return address
-	addiu 	$fp, $sp, 20	# setup updateSnake frame pointer	
+	addiu 	$fp, $sp, 20	# setup updateShark frame pointer	
 	
 	lw	$t3, xVel	# load xVel from memory
 	lw	$t4, yVel	# load yVel from memory
@@ -317,27 +317,27 @@ updateSnakeHeadPosition:
 exitMoving:
 	j 	gameUpdateLoop		# loop back to beginning
 
-# this function draws the apple base upon x and y coordintes
+# this function draws the Plastic base upon x and y coordintes
 # code logic
-# drawApple() {
+# drawPlastic() {
 #	convert (x, y) to bitmap display
 #	store red color into bitmap display
-#	exit drawApple
+#	exit drawPlastic
 # }
-drawApple:
+drawPlastic:
 	addiu 	$sp, $sp, -24	# allocate 24 bytes for stack
 	sw 	$fp, 0($sp)	# store caller's frame pointer
 	sw 	$ra, 4($sp)	# store caller's return address
-	addiu 	$fp, $sp, 20	# setup updateSnake frame pointer
+	addiu 	$fp, $sp, 20	# setup updateShark frame pointer
 	
-	lw	$t0, appleX		# t0 = xPos of apple
-	lw	$t1, appleY		# t1 = yPos of apple
+	lw	$t0, plasticX		# t0 = xPos of plastic
+	lw	$t1, plasticY		# t1 = yPos of plastic
 	lw	$t2, yConversion	
 	lw	$t3, xConversion	
-	mult	$t1, $t2		# appleY * 128
-	mflo	$t4			# t4 = appleY * 128
+	mult	$t1, $t2		# plasticY * 128
+	mflo	$t4			# t4 = plasticY * 128
 	mult	$t0, $t3			
-	mflo	$t5 			# t5 = appleX * 4
+	mflo	$t5 			# t5 = plasticX * 4
 	add	$t0, $t4, $t5		
 		
 	la 	$t1, frameBuffer	# load frame buffer address
@@ -350,54 +350,54 @@ drawApple:
 	addiu 	$sp, $sp, 24	# restores caller's stack pointer
 	jr 	$ra		# return to caller's code	
 
-# This function finds a new spot for an apple after its been eaten
+# This function finds a new spot for an plastic after its been eaten
 # does so randomly using syscall 42 which is a random number generator
 # code logic:
-# newAppleLocation() {
+# newplasticLocation() {
 #	get random X from 0 - 63
 # 	get random Y from 0 - 31
 #	convert (x, y) to bit map display value
 # 	if (bit map display value != gray background)
 #		redo the randomize
-#	once good apple spot found store x, y in memory
-#	exit newAppleLocation
+#	once good plastic spot found store x, y in memory
+#	exit newplasticLocation
 # }
-newAppleLocation:
+newPlasticLocation:
 	addiu 	$sp, $sp, -24	# allocate 24 bytes for stack
 	sw 	$fp, 0($sp)	# store caller's frame pointer
 	sw 	$ra, 4($sp)	# store caller's return address
-	addiu 	$fp, $sp, 20	# setup updateSnake frame pointer
+	addiu 	$fp, $sp, 20	# setup updateShark frame pointer
 
 redoRandom:		
 	addi	$v0, $zero, 42	# random int 
 	addi	$a1, $zero, 31	# upper bound
 	syscall
-	add	$t1, $zero, $a0	# random appleX
+	add	$t1, $zero, $a0	# random PlasticX
 	
 	addi	$v0, $zero, 42	# random int 
 	addi	$a1, $zero, 31	# upper bound
 	syscall
-	add	$t2, $zero, $a0	# random appleY
+	add	$t2, $zero, $a0	# random PlasticY
 	
 	lw	$t3, yConversion	
 	mult	$t2, $t3		
-	mflo	$t4			# t4 = random appleY * 128
+	mflo	$t4			# t4 = random PlasticY * 128
 	lw 	$t3, xConversion
 	mult	$t3, $t1
-	mflo	$t5			# t5 = random appleX * 4
-	add	$t4, $t4, $t5		# t4 = random appleY * 64 + random appleX *4
+	mflo	$t5			# t5 = random PlasticX * 4
+	add	$t4, $t4, $t5		# t4 = random PlasticY * 64 + random PlasticX *4
 	
 	la 	$t0, frameBuffer	# load frame buffer address
 	add	$t0, $t4, $t0		
 	lw	$t5, 0($t0)		# t5 = value of pixel at t0
 	
 	li	$t6, 0xFF003366		# load light gray color
-	beq	$t5, $t6, goodApple	# if loction is a good sqaure branch to goodApple
+	beq	$t5, $t6, goodPlastic	# if loction is a good sqaure branch to goodPlastic
 	j redoRandom
 
-goodApple:
-	sw	$t1, appleX
-	sw	$t2, appleY	
+goodPlastic:
+	sw	$t1, plasticX
+	sw	$t2, plasticY	
 
 	lw 	$ra, 4($sp)	# load caller's return address
 	lw 	$fp, 0($sp)	# restores caller's frame pointer
